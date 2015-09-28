1e030df20af0e36de72c6f71b105a644a3f3aafe
Ninjestra <joel.lindqvist@boldarc.com>
2015-09-28 17:49:10 +0200

    Objects separated, normals exported
    Objects are still centered around origin, as transforms are applied
    directly to vertices.

--- BoldarcExportContext.cs
+++ BoldarcExportContext.cs
@@ -14,27 +14,111 @@ namespace BoldarcRevitPlugin
     class BoldarcExportContext : IExportContext
     {
         private FbxExporter m_Exporter;
+        private Stack<Transform> m_stackTransform;
+        private Stack<ElementId> m_stackElementId = new Stack<ElementId>();
+        private Document m_Document;
+
+        ElementId CurrentElementId
+        {
+            get
+            {
+                return (m_stackElementId.Count > 0)
+                  ? m_stackElementId.Peek()
+                  : ElementId.InvalidElementId;
+            }
+        }
+
+        Element CurrentElement
+        {
+            get
+            {
+                return m_Document.GetElement(
+                  CurrentElementId);
+            }
+        }
+
         public BoldarcExportContext(Document inDocument) 
         {
             AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
+            m_Document = inDocument;
             m_Exporter = new FbxExporter();
+            m_stackTransform = new Stack<Transform>();
+            m_stackTransform.Push(Transform.Identity);
         }
         public void OnPolymesh(PolymeshTopology inMesh)
         {
-            
-            //Assembly.LoadFrom(asmLocation);
-            m_Exporter.AddGeometry(inMesh.GetPoints(), inMesh.GetFacets());
-            
+            switch (inMesh.DistributionOfNormals)
+            {
+                case DistributionOfNormals.AtEachPoint:
+                    break;
+
+                case DistributionOfNormals.OnEachFacet:
+                    break;
+
+                case DistributionOfNormals.OnePerFace:
+                    break;
+
+                default:
+                    break;
+            }
+            m_Exporter.AddGeometry(inMesh.GetPoints(), inMesh.GetFacets(), inMesh.GetNormals(), m_stackTransform.Peek());
+
         }
-        public bool Start() { return true; }
-        public void Finish() { m_Exporter.ExportFbx(); }
+        public bool Start() 
+        {
+
+            return true; 
+        }
+        public void Finish() 
+        {
+            m_Exporter.ExportFbx();
+            //Process.Start(@"D:\Program Files\Blender Foundation\Blender\blender.exe", @"-b --python D:\Joel\Scripts\python\Blender\RevitUnrealBridge.py -- D:\Joel\FBX\exporter_test.fbx");
+        }
         public bool IsCanceled() { return false; }
         public RenderNodeAction OnViewBegin(ViewNode inNode) { return 0; }
         public void OnViewEnd(ElementId inID) { }
-        public RenderNodeAction OnElementBegin(ElementId inID) { return 0; }
-        public void OnElementEnd(ElementId inID) { }
-        public RenderNodeAction OnInstanceBegin(InstanceNode inID) { return 0; }
-        public void OnInstanceEnd(InstanceNode inID) { }
+        public RenderNodeAction OnElementBegin(ElementId inID)
+        {
+            m_stackElementId.Push(inID);
+        //    Category category = CurrentElement.get_Category();
+            //category.CategoryType == CategoryType.
+            //bool _b = category != null && (category.get_Id().get_IntegerValue().Equals(-2001320) || category.get_Id().get_IntegerValue().Equals(-2000175) || category.get_Id().get_IntegerValue().Equals(-2000126));
+          //  if ()
+          //  {
+                //this.ExportSolids(element);
+         //       return RenderNodeAction.Skip;
+         //   }
+           // if (this.IsElementDecal(element))
+            //{
+            //    //this.ExportDecal(element);
+             //   return RenderNodeAction.Skip;
+           // }
+         //   if (CurrentElement.Category == )
+          //  {
+            String _name = CurrentElement.Category.Name + "_0_" + CurrentElement.Name;
+            m_Exporter.BeginMesh(_name);
+            return RenderNodeAction.Proceed;
+          //  }
+           // else
+            //    return RenderNodeAction.Skip;
+           
+            
+        }
+        public void OnElementEnd(ElementId inID) 
+        {
+            //if (CurrentElement.Category.CategoryType == CategoryType.Model)
+            m_Exporter.EndMesh();
+            m_stackElementId.Pop();
+        }
+        public RenderNodeAction OnInstanceBegin(InstanceNode inNode) 
+        {
+            m_stackTransform.Push(m_stackTransform.Peek().Multiply(inNode.GetTransform()));
+            return RenderNodeAction.Proceed; 
+        }
+        public void OnInstanceEnd(InstanceNode inNode) 
+        {
+            m_stackTransform.Pop();
+        }
         public RenderNodeAction OnLinkBegin(LinkNode inNode) { return 0; }
         public void OnLinkEnd(LinkNode inNode) { }
         public RenderNodeAction OnFaceBegin(FaceNode inNode) { return 0; }
--- FbxExporter.cs
+++ FbxExporter.cs
@@ -8,16 +8,24 @@ namespace BoldarcRevitPlugin
     class FbxExporter
     {
         private FileExporter m_pFileExporter;
+        private BoldarcManagedFbx.Mesh m_pCurrentMesh;
         public FbxExporter()
         {
             m_pFileExporter = new FileExporter();
         }
-        public void AddGeometry(IList<XYZ> inVertices, IList<PolymeshFacet> inIndices)
+
+        public void BeginMesh(String inName)
+        {
+            m_pCurrentMesh = new BoldarcManagedFbx.Mesh(inName);
+        }
+
+        public void AddGeometry(IList<XYZ> inVertices, IList<PolymeshFacet> inIndices, IList<XYZ> inNormals, Transform inTransform)
         {
             List<Vector3> _lVertices = new List<Vector3>(inVertices.Count);
             foreach (XYZ _vert in inVertices)
             {
-                _lVertices.Add(new Vector3(_vert.X, _vert.Y, _vert.Z));
+                XYZ _newVert = inTransform.OfPoint(_vert);
+                _lVertices.Add(new Vector3(_newVert.X, _newVert.Y, _newVert.Z));
             }
 
             List<int> _lIndices = new List<int>(inIndices.Count * 3);
@@ -27,12 +35,25 @@ namespace BoldarcRevitPlugin
                 _lIndices.Add(_ind.V2);
                 _lIndices.Add(_ind.V3);
             }
-            //Mesh _Mesh = new Mesh();
-            //_Mesh.AddGeometry(_lVertices, _lIndices);
-            //m_pFileExporter.AddMesh(_Mesh);
-            m_pFileExporter.AddGeometry(_lVertices, _lIndices);
+
+            List<Vector3> _lNormals = new List<Vector3>(inNormals.Count);
+            foreach (XYZ _norm in inNormals)
+            {
+                //XYZ _newNorm = inTransform.OfPoint(_norm);
+                _lNormals.Add(new Vector3(_norm.X, _norm.Y, _norm.Z));
+            }
+
+            m_pCurrentMesh.AddGeometry(_lVertices, _lIndices, _lNormals);
             
         }
+
+        public void EndMesh()
+        {
+            m_pCurrentMesh.OptimizePoints();
+            if (!m_pCurrentMesh.IsEmpty())
+                m_pFileExporter.AddMesh(m_pCurrentMesh);
+        }
+
         public void ExportFbx()
         {
             m_pFileExporter.ExportFile();
